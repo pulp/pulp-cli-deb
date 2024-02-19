e0b9fd38ad739821c82fa84466e6fa740ab4cf14
pulpbot <pulp-infra@redhat.com>
2024-02-19 16:27:52 +0000

    Bump version: 0.0.7 → 0.0.8.dev

--- pulpcore/cli/deb/__init__.py
+++ pulpcore/cli/deb/__init__.py
@@ -9,7 +9,7 @@ from pulpcore.cli.deb.publication import publication
 from pulpcore.cli.deb.remote import remote
 from pulpcore.cli.deb.repository import repository
 
-__version__ = "0.0.7"
+__version__ = "0.0.8.dev"
 
 
 @pulp_group(name="deb")
