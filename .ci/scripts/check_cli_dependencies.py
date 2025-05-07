9616bde7458091795c7df2c9f4f8884d57cb1782
Matthias Dellweg <2500@gmx.de>
2025-05-07 15:53:50 +0200

    Update from cookiecutter template

---
+++ .ci/scripts/check_cli_dependencies.py
@@ -0,0 +1,29 @@
+#!/bin/env python3
+import tomllib
+import typing as t
+from pathlib import Path
+
+from packaging.requirements import Requirement
+
+GLUE_DIR = "pulp-glue-deb"
+
+
+def dependencies(path: Path) -> t.Iterator[Requirement]:
+    with (path / "pyproject.toml").open("rb") as fp:
+        pyproject = tomllib.load(fp)
+
+    return (Requirement(r) for r in pyproject["project"]["dependencies"])
+
+
+if __name__ == "__main__":
+    base_path = Path(__file__).parent.parent.parent
+    glue_path = base_path / GLUE_DIR
+
+    cli_dependency = next((r for r in dependencies(base_path) if r.name == "pulp-cli"))
+    glue_dependency = next((r for r in dependencies(glue_path) if r.name == "pulp-glue"))
+
+    if cli_dependency.specifier != glue_dependency.specifier:
+        print("🪢 CLI and GLUE dependencies mismatch:")
+        print(" ", cli_dependency)
+        print(" ", glue_dependency)
+        exit(1)
