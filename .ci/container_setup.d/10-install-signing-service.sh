334afd29b476193cd5502106a9bd9bb2c2d4b8ee
Balasankar 'Balu' C <balu@dravidam.net>
2025-10-19 13:12:18 +0530

    Add signing-service as an argument to repository creation and updation
    closes #54
    
    Signed-off-by: Balasankar 'Balu' C <balu@dravidam.net>

---
+++ .ci/container_setup.d/10-install-signing-service.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -eu
+
+pulp --config "${PULP_CLI_CONFIG}" debug has-plugin --name "deb" && HAS_DEB=true || HAS_DEB=""
+if [ "$HAS_DEB" ]
+then
+  if [ ! -f pytest_pulp_cli/GPG-PRIVATE-KEY-fixture-signing ]
+  then
+    curl -L https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-PRIVATE-KEY-fixture-signing > pytest_pulp_cli/GPG-PRIVATE-KEY-fixture-signing
+  fi
+  echo "Setup the signing services"
+  if "${CONTAINER_RUNTIME}" exec "pulp-ephemeral" id pulp
+  then
+    PULP_USER="pulp"
+  else
+    PULP_USER="root"
+  fi
+  "${CONTAINER_RUNTIME}" exec "pulp-ephemeral" mkdir -p /var/lib/pulp/scripts/
+  # Setup key on the Pulp container
+  echo "0C1A894EBB86AFAE218424CADDEF3019C2D4A8CF:6:" | "${CONTAINER_RUNTIME}" exec -i "pulp-ephemeral" su "${PULP_USER}" -c "gpg --import-ownertrust"
+  "${CONTAINER_RUNTIME}" exec -i "pulp-ephemeral" su "${PULP_USER}" -c "gpg --import" < pytest_pulp_cli/GPG-PRIVATE-KEY-fixture-signing
+  if [ "$HAS_DEB" ]
+  then
+    echo "Setup deb release signing service"
+    "${CONTAINER_RUNTIME}" exec -i "pulp-ephemeral" bash -c "cat > /var/lib/pulp/scripts/sign_deb_release.sh" < "${BASEPATH}/../tests/assets/sign_deb_release.sh"
+    "${CONTAINER_RUNTIME}" exec "pulp-ephemeral" chmod a+x /var/lib/pulp/scripts/sign_deb_release.sh
+    "${CONTAINER_RUNTIME}" exec "pulp-ephemeral" su "${PULP_USER}" -c "pulpcore-manager add-signing-service --class deb:AptReleaseSigningService sign_deb_release /var/lib/pulp/scripts/sign_deb_release.sh 'pulp-fixture-signing-key'"
+  fi
+fi
